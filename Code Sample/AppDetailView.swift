e8e1ac92faacb87c0d3758bd244b8925a8b9a71c
Consie <consie@conorms-macbook-pro.local>
2017-02-14 01:39:49 -0500

    Second, awful commit

---
+++ Code Sample/AppDetailView.swift
@@ -0,0 +1,28 @@
+//
+//  AppDetailView.swift
+//  Code Sample
+//
+//  Created by Consie on 14/02/2017.
+//  Copyright © 2017 Conor. All rights reserved.
+//
+
+import UIKit
+
+//http://cdn.akamai.steamstatic.com/steam/apps/APPID/header.jpg
+
+class AppDetailView :UIViewController {
+    
+    var appData = AppDataModel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
